05746f64f96316b3ac3898bbbae64b4b05f388b1
Husniddin <husanovhusniddin492@gmail>
2022-11-19 01:42:37 +0500

    tugadi

---
+++ app.js
@@ -0,0 +1,28 @@
+let key = prompt("Assalomu Aleykum Xush kelibsiz!"); 
+console.log(key);
+
+ key = prompt("O'zingiz haqingizda ma'lumot bersangiz"); 
+ console.log(key);
+
+ key = prompt("Ismingiz?"); 
+ console.log(key);
+
+ key = prompt("Familyangiz?"); 
+ console.log(key);
+
+ key = prompt("Yoshingiz?"); 
+ console.log(key);
+
+ key = prompt("Js yoqdimi?"); 
+ console.log(key);
+
+ key = prompt("Menga savolingiz bormi"); 
+
+console.log(key);
+
+
+
+// console.log(person[key]);
+
+
+document.querySelector("h1").innerText = product[key];
